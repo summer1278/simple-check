8abd1c2bc6fe18c914d7488b0176cd828912961e
Xia Cui <cxcui188@gmail.com>
2014-08-08 16:12:58 +0100

    encoding problem unfixed

--- lib/checker/simple_collecter.rb
+++ lib/checker/simple_collecter.rb
@@ -1,6 +1,8 @@
 require 'csv'
+require 'pp'
+# coding:utf-8
 module Checker
-  
+
   class SimpleCollecter
 	def initialize ( source_dir, translation_dir, result_dir)
 	 @source_dir = source_dir
@@ -9,15 +11,37 @@ module Checker
 	end
 
 	def collect
-	  	
+	  	collect_source
+    end
+    
+    def group_both
+    	
     end
 
     def collect_source
-	  	
-	end
+    
+      sentences = [] 
+	  Dir.glob( @source_dir +'/*.txt') do |doc_file|
+	  	data = []
+	  	data[2] = File.basename(doc_file)
+        lines = File.readlines(doc_file)
+        if lines != nil
+          lines.each_with_index do |line, index|
+            if index%2 == 0
+           	  data[0] = line
+            else
+          	  data[1] = line
+          	  sentences << data
+            end
+          end
+        end
+        pp sentences
+     end
+
+    end
 
 	def collect_translation
-	  	
+
 	end
 
 
