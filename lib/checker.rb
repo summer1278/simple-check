d911543490c95f7de18f1974be80b347d30942d0
Xia Cui <cxcui188@gmail.com>
2014-08-08 13:07:06 +0100

    initial commit

---
+++ lib/checker.rb
@@ -0,0 +1,1 @@
+require 'checker/simple_collecter'
---
+++ lib/checker/simple_collecter.rb
@@ -0,0 +1,25 @@
+require 'csv'
+module Checker
+  
+  class SimpleCollecter
+	def initialize ( source_dir, translation_dir, result_dir)
+	 @source_dir = source_dir
+	 @translation_dir = translation_dir
+	 @result_dir = result_dir
+	end
+
+	def collect
+	  	
+    end
+
+    def collect_source
+	  	
+	end
+
+	def collect_translation
+	  	
+	end
+
+
+  end
+end
