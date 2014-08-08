a766b0284a5a20667f9e56bd66831eea7a4a989d
Xia Cui <cxcui188@gmail.com>
2014-08-09 00:36:43 +0100

    basic function complete

--- lib/checker/simple_collecter.rb
+++ lib/checker/simple_collecter.rb
@@ -4,46 +4,60 @@ require 'pp'
 module Checker
 
   class SimpleCollecter
-	def initialize ( source_dir, translation_dir, result_dir)
-	 @source_dir = source_dir
-	 @translation_dir = translation_dir
-	 @result_dir = result_dir
-	end
+   def initialize ( source_dir, translation_dir, result_dir)
+    @source_dir = source_dir
+    @translation_dir = translation_dir
+    @result_dir = result_dir
+  end
 
-	def collect
-	  	collect_source
-    end
-    
-    def group_both
-    	
-    end
+  def collect
+    group_source_and_translation
+  end
 
-    def collect_source
-    
-      sentences = [] 
-	  Dir.glob( @source_dir +'/*.txt') do |doc_file|
-	  	data = []
+  def group_source_and_translation
+   src = collect_dir(@source_dir)
+   trs = collect_dir(@translation_dir)
+   CSV.open('test/data.csv', 'w') do |csv_file|
+    src.each_with_index {|row,i| csv_file<<row.insert(2,trs[i][1])}
+   end
+ end
+
+ def collect_dir (source)
+
+  sentences = [] 
+  data = Array.new(3,nil)
+  Dir.glob( source +'/*.txt') do |doc_file|
+	  	#data = Array.new(3,nil)
 	  	data[2] = File.basename(doc_file)
-        lines = File.readlines(doc_file)
-        if lines != nil
-          lines.each_with_index do |line, index|
-            if index%2 == 0
-           	  data[0] = line
+      file = File.open(doc_file, 'r')
+      lines = file.readlines
+
+      if lines != nil
+        lines.each_with_index do |line, index|
+          if index % 2 == 0
+            data[0] = remove_symbols(line)
+              #p data[0]
             else
-          	  data[1] = line
-          	  sentences << data
+             data[1] = remove_symbols(line)
+              #p data
+              sentences << [data[0],data[1],data[2]]
             end
+            #sentences << data
           end
         end
-        pp sentences
-     end
-
-    end
-
-	def collect_translation
-
-	end
-
+        
+      end
+      return sentences
+     #CSV.open('test/data.csv', 'w') do |csv_file|
+    #  sentences.each {|row| csv_file<<row}
+    #end
 
   end
+
+  def remove_symbols(string)
+    string.gsub('/\r?\n/','').gsub("\\n","\n").strip
+  end
+
+
+end
 end
